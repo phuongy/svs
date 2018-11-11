ea96169f74e438b81abda0f2313e4f69715587fa
Phuong Huynh <huynhpt@gmail.com>
2018-11-11 23:29:24 +1100

    add gatsby and svs

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { SVSCalculator } from '../components/svs'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <SVSCalculator />
+  </Layout>
+)
+
+export default IndexPage
