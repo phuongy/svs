6bad49046c8fc466fc81ed4c174a109e47c403f1
Phuong Huynh <huynhpt@gmail.com>
2018-11-12 07:58:27 +1100

    add svs page

---
+++ src/pages/svs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SVSCalculator } from '../components/svs'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <SVSCalculator />
+  </Layout>
+)
+
+export default IndexPage
